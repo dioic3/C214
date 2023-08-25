9d3d33189b478a10f6cf9dc29520f7cfc66c0067
joyce-4i <j.dias@4intelligence.com.br>
2023-08-25 20:03:11 -0300

    aula 2

--- desafio.js
+++ desafio.js
@@ -7,7 +7,20 @@ class Filme {
         this.assistido = assistido
         this.avaliacao = avaliacao
     }
-
+    //Exibir um menu
+    exibirMenu() {
+        console.log("Menu:");
+        console.log("1. Adicionar um novo filme");
+        console.log("2. Marcar um filme como assistido");
+        console.log("3. Avaliar um filme");
+        console.log("4. Exibir a lista de filmes");
+        console.log("5. Sair do programa");
+    }
+    // Função para adicionar um novo filme
+    adicionarFilme() {
+        const nomeDoFilme = prompt("Digite o nome do filme:");
+        return nomeDoFilme;
+    }
     //Exibir detalhes do filme 
     DetalhesFilme() {
         console.log("Titulo do Filme:", this.titulo)
@@ -17,7 +30,7 @@ class Filme {
         console.log("Se foi assistido:", this.assistido)
         console.log("Qual é a avaliação?", this.avaliacao)
     }
-
+    //identificar se o filme está com assistido
     Assistir(assistido) {
         if (assistido == "SIM" || assistido == "sim") {
             console.log("Foi assistido")
@@ -26,25 +39,73 @@ class Filme {
             console.log("Não foi assistido")
         }
     }
-
+    //função para avaliar o filme
     AvaliarFilme(nota) {
         this.avaliacao = nota
         return nota
     }
+    //função para marcar o filme como assistido
+    marcarComoAssistido() {
+        exibirListaDeFilmes();
+        const indiceDoFilme = prompt("Digite o número do filme que deseja marcar como assistido:")
+
+        if (indiceDoFilme == "assistido") {
+            console.log("Seu filme foi marcado como assistido!")
+            this.assistido = indiceDoFilme;
+        } else {
+            console.log("O filme foi marcado como não assistido");
+        }
+    }
+    //Função para avaliar o filme
+    avaliarFilme() {
+        exibirListaDeFilmes();
+        const indiceDoFilme = parseInt(prompt("Digite o número do filme que deseja avaliar:")) - 1;
+
+        if (indiceDoFilme >= 0 && indiceDoFilme < listaDeFilmes.length) {
+            const avaliacao = parseFloat(prompt("Digite a avaliação (de 1 a 5) para o filme:"));
+            if (avaliacao >= 1 && avaliacao <= 5) {
+                listaDeFilmes[indiceDoFilme].avaliacao = avaliacao;
+                console.log("Filme avaliado com sucesso!");
+            } else {
+                console.log("Avaliação inválida! Deve ser um valor entre 1 e 5.");
+            }
+        } else {
+            console.log("Número de filme inválido!");
+        }
+    }
+    //função para exibir os filmes
+    exibirListaDeFilmes() {
+        console.log("\nLista de Filmes:");
+        listaDeFilmes.forEach((filme, index) => {
+            console.log(`${index + 1}. ${filme.nome} - Assistido: ${filme.assistido ? 'Sim' : 'Não'} - Avaliação: ${filme.avaliacao || 'Não avaliado'}`);
+        });
+    }
 }
 
-const filme1 = new Filme("Joyce", 1400, "Romance", "1 minuto", "sim", 5);
-const filme2 = new Filme("Narnia", 2002, "Ficção", "2 horas", "sim", 9)
-const filme3 = new Filme("Harry Potter", 2010, "Fantasia", "3 horas", "não", 8)
+const listaDeFilmes = new Filme;
+let escolha = 0;
+do {
+    listaDeFilmes.exibirMenu();
+    escolha = parseInt(prompt("Escolha uma opção:"));
 
-filme1.DetalhesFilme();
-filme1.Assistir();
-console.log(filme1.AvaliarFilme(10))
+    switch (escolha) {
+        case 1:
+            listaDeFilmes.adicionarFilme();
+            break;
+        case 2:
+            listaDeFilmes.marcarComoAssistido();
+            break;
+        case 3:
+            listaDeFilmes.avaliarFilme();
+            break;
+        case 4:
+            listaDeFilmes.exibirListaDeFilmes();
+            break;
+        case 5:
+            console.log("Saindo do programa. Até logo!");
+            break;
+        default:
+            console.log("Opção inválida! Escolha uma opção válida.");
+    }
+} while (escolha !== 5);
 
-console.log(" ------------------ ")
-filme2.DetalhesFilme();
-filme2.Assistir();
-
-console.log(" ----------------- ")
-filme3.DetalhesFilme();
-filme3.Assistir();
---
+++ readMe.md
@@ -0,0 +1,3 @@
+# C214 - Engenharia de Software
+
+- O projeto tem a finalidade de ensinar os fundamentos de javascript e como subir um projeto ao github, usando controle de versionamento.
